3c2bca4a0d3f6e0087d43eca60f7248fbf3b1048
o.dvolyatik <o.dvolyatik@gmail.com>
2021-08-22 03:48:00 +0700

    add new comics

--- shared-spot/src/main/java/ru/endroad/hakaton/shared/spot/entity/Comics.kt
+++ shared-spot/src/main/java/ru/endroad/hakaton/shared/spot/entity/Comics.kt
@@ -4,5 +4,6 @@ import ru.endroad.hakaton.shared.spot.R
 
 enum class Comics(val title: String, val image: Image, val position: Position) {
 	IVAN_GROZNIY("Иван Грозный перестает быть грозным", Image(R.drawable.comics_ivan), Position(58.1274491, 30.3825126)),
-	PARASKEEVA("Параскеева домоседка", Image(R.drawable.comics_icona), Position(58.6125113, 33.6861279))
+	PARASKEEVA("Параскеева домоседка", Image(R.drawable.comics_icona), Position(58.6125113, 33.6861279)),
+	ALENY_STONE("Алёнушкин камень", Image(R.drawable.comics_alena_stone), Position(58.456310, 32.454366)),
 }
